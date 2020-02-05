3686cb9defb3e56fcc77d178309751c1da5230a7
ydzydzydz <a25112512@gmail.com>
2020-02-06 01:41:56 +0800

    update

--- shell_16.sh
+++ shell_16.sh
@@ -2,18 +2,23 @@
 
 #------------------------------
 # 检测网站是否正常运行
+# bash shell_16.sh url.list
 #------------------------------
 
 INFO=`echo -e "\033[32m[ 正常 ]\033[0m"`
-ERRO=`echo -e "\033[31m[ 挂了 ]\033[0m"`
-WARN=`echo -e "\033[33m[ 提示 ]\033[0m"`
+ERRO=`echo -e "\033[31m[ 异常 ]\033[0m"`
+WARN=`echo -e "\033[33m[ 警告 ]\033[0m"`
+TIME=`date "+%Y-%m-%d %H:%M:%S"`
 
 if [ $# -eq 0 ]; then
-	echo "$WARN 请输入参数" 1>&2 && exit 1
+	echo "$WARN 请输入文件" 1>&2 && exit 1
+elif [ ! -f $1 ]; then
+	echo "$WARN 文件不存在" 1>&2 && exit 1
 else
-	for i in $*
+	for i in `cat $1`
 	do
 		curl -s $i -o /dev/null
-		[ $? -eq 0 ] && echo "$INFO $i" || echo "$ERRO $i" 1>&2
+		[ $? -eq 0 ] && echo "$INFO $i $TIME" || echo "$ERRO $i $TIME" 1>&2
 	done
 fi
+
