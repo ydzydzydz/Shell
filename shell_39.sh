d928ec4b65cc81151d7f4233cb11780e1e790280
ydzydzydz <a25112512@gmail.com>
2021-05-23 21:06:44 +0800

    update

--- shell_39.sh
+++ shell_39.sh
@@ -4,25 +4,25 @@
 # 判断github脚本更新
 #-------------------------
 
-sh_ver=v0.0.2
-sh_name=shell_39.sh
+sh_ver=v0.0.3
+sh_name=${0##*/}
 
 gh_user=ydzydzydz
 gh_repo=Shell
 gh_branch=master
 
-erro=$(echo -e "\033[31m[ERRO]\033[0m $(date "+%Y-%m-%d %H:%M:%S") -")
-info=$(echo -e "\033[32m[INFO]\033[0m $(date "+%Y-%m-%d %H:%M:%S") -")
-warn=$(echo -e "\033[33m[WARN]\033[0m $(date "+%Y-%m-%d %H:%M:%S") -")
+erro=$(echo -e "\033[31m[ERRO]\033[0m $(date "+%Y-%m-%d %H:%M:%S") - ${sh_name}:")
+info=$(echo -e "\033[32m[INFO]\033[0m $(date "+%Y-%m-%d %H:%M:%S") - ${sh_name}:")
+warn=$(echo -e "\033[33m[WARN]\033[0m $(date "+%Y-%m-%d %H:%M:%S") - ${sh_name}:")
 
 check_update () {
 	sh_new_ver=$(wget -qO- https://raw.githubusercontent.com/${gh_user}/${gh_repo}/${gh_branch}/${sh_name} | grep "sh_ver" | head -n 1 | awk -F= '{print $2}')
 	if [[ -z "$sh_new_ver" ]]; then 
-		echo -e "$erro $sh_name: 获取更新失败"
+		echo -e "$erro 获取更新失败"
 	elif [[ "$sh_ver" == "$sh_new_ver" ]]; then
-		echo -e "$info $sh_name: 当前版本 \033[36m${sh_ver}\033[0m, 无可用更新"
+		echo -e "$info 当前版本 \033[36m${sh_ver}\033[0m, 无可用更新"
 	else
-		echo -e "$info $sh_name: 当前版本 \033[36m${sh_ver}\033[0m, 有可用更新 \033[32m${sh_new_ver}\033[0m"
+		echo -e "$info 当前版本 \033[36m${sh_ver}\033[0m, 有可用更新 \033[32m${sh_new_ver}\033[0m"
 		echo -e "$warn 获取更新: wget https://raw.githubusercontent.com/${gh_user}/${gh_repo}/${gh_branch}/${sh_name}"
 		echo -e "$warn 执行权限: chmod +x $sh_name"
 		echo -e "$warn 执行脚本: ./$sh_name"
