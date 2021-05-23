9e437ad85f1f71773a4f34309019d6055f160f4d
ydzydzydz <a25112512@gmail.com>
2021-05-23 20:50:31 +0800

    update

--- shell_39.sh
+++ shell_39.sh
@@ -4,7 +4,7 @@
 # 判断github脚本更新
 #-------------------------
 
-sh_ver=v0.0.1
+sh_ver=v0.0.2
 sh_name=shell_39.sh
 
 gh_user=ydzydzydz
@@ -20,11 +20,12 @@ check_update () {
 	if [[ -z "$sh_new_ver" ]]; then 
 		echo -e "$erro $sh_name: 获取更新失败"
 	elif [[ "$sh_ver" == "$sh_new_ver" ]]; then
-		echo -e "$info $sh_name: 无可用更新(\033[32m${sh_new_ver}\033[0m)"
+		echo -e "$info $sh_name: 当前版本 \033[36m${sh_ver}\033[0m, 无可用更新"
 	else
-		echo -e "$info $sh_name: 有可用更新(\033[32m${sh_new_ver}\033[0m)"
+		echo -e "$info $sh_name: 当前版本 \033[36m${sh_ver}\033[0m, 有可用更新 \033[32m${sh_new_ver}\033[0m"
 		echo -e "$warn 获取更新: wget https://raw.githubusercontent.com/${gh_user}/${gh_repo}/${gh_branch}/${sh_name}"
 		echo -e "$warn 执行权限: chmod +x $sh_name"
+		echo -e "$warn 执行脚本: ./$sh_name"
 		exit 1
 	fi
 }
